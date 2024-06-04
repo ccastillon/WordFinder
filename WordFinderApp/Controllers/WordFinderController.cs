cde1c37c52025ba2f1a8536f3f3cc230bd8b7d5f
Ched Castillon <chedcastillondev@gmail.com>
2024-06-04 15:29:31 +0800

    Initial commit

---
+++ WordFinderApp/Controllers/WordFinderController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WordFinderApp.Models;
+
+namespace WordFinderApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordFinderController : ControllerBase
+    {
+        [HttpPost("find")]
+        public ActionResult<IList<string>> FindWords([FromBody] WordFinderRequest request)
+        {
+            var wordFinder = new WordFinder(request.Dictionary);
+            var result = wordFinder.Find(request.Matrix);
+            return Ok(result);
+        }
+    }
+
+    public class WordFinderRequest
+    {
+        public IEnumerable<string> Dictionary { get; set; }
+        public IEnumerable<string> Matrix { get; set; }
+    }
+}
---
+++ WordFinderApp/Models/WordFinder.cs
@@ -0,0 +1,53 @@
+﻿namespace WordFinderApp.Models
+{
+    public class WordFinder
+    {
+        private readonly HashSet<string> _dictionary;
+
+        public WordFinder(IEnumerable<string> dictionary)
+        {
+            _dictionary = new HashSet<string>(dictionary);
+        }
+
+        public IList<string> Find(IEnumerable<string> src)
+        {
+            var foundWords = new HashSet<string>();
+            var matrix = src.ToArray();
+            int rows = matrix.Length;
+            int cols = matrix[0].Length;
+
+            // Search horizontally
+            foreach (var word in _dictionary)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    if (matrix[i].Contains(word))
+                    {
+                        foundWords.Add(word);
+                    }
+                }
+            }
+
+            // Search vertically
+            for (int col = 0; col < cols; col++)
+            {
+                string colString = string.Empty;
+                for (int row = 0; row < rows; row++)
+                {
+                    colString += matrix[row][col];
+                }
+
+                foreach (var word in _dictionary)
+                {
+                    if (colString.Contains(word))
+                    {
+                        foundWords.Add(word);
+                    }
+                }
+            }
+
+            return foundWords.ToList();
+        }
+    }
+}
+
